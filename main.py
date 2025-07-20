5b8d82a0ead4c8629d92defe2187f359323af6c4
Roskova Marcoraz <bjrk1992@gmail.com>
2025-07-20 19:22:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from stash_api import get_scenes, get_scene_meta, search_scenes, search_performe
 
 addon_manifest = {
     "id": "org.stremio.stashdb",
-    "version": "1.1.1", # Version bump
+    "version": "1.1.2", # Version bump
     "name": "StashDB Catalog",
     "description": "Provides an adult content catalog from StashDB.org for Stremio.",
     "resources": ["catalog", "meta"],
